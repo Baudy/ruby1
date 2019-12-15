edeafe99a2fd192109c2196d458be81b23d366bc
Tim Carey <tc@timcarey.net>
2019-12-15 09:13:20 +0000

    added all files

--- calc.rb
+++
@@ -1,6 +0,0 @@
-puts "Enter a number: "
-num1 = gets.chomp().to_f #need to convert to int or float otherwise it'll concat
-puts "Enter another number: "
-num2 = gets.chomp().to_f
-
-puts (num1 + num2)
--- user_input.rb
+++
@@ -1,3 +0,0 @@
-puts "Enter your name: "
-name = gets.chomp() #chomp removes the carriage return from my input
-puts ("Hello, " + name + ", you are cool")
